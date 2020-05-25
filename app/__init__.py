922b93521118e73f858f4b935ae209d2126165a8
ronaldocerdras <ronaldocedras@gmail.com>
2020-05-25 19:41:29 +0200

    done with navBar and intro to the website

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_mail import Mail, Message
+import smtplib
+
 
 app = Flask(__name__)
+
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'rcedras0010@gmail.com',
+    MAIL_PASSWORD = 'insert_enviorment_variable'
+)
+
+mail = Mail(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
-from app import app 
+from app import app, mail
+from flask_mail import Mail, Message
+from flask import Flask, render_template, request
 
-@app.route('/')
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return 'hello world!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return '<h1>Hello about</h1>'
+
+@app.route('/sent', methods = ['POST', 'GET'])
+def send():
+    result = request.form
+    sent = str(result['sender'])
+    email = str(result['mail'])
+
+    msg = Message('Mail from your Website', sender='ronaldocedras@gmail.com', recipients=['rcedras0010@gmail.com'] )
+    msg.body = email + '\nFrom ' + sent
+    mail.send(msg)
+
+    return '<h1>Message sent</h1>'
