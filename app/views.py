1099e61e5bc8581aec75e10595ae74a8f7d3a8fe
ronaldocerdras <ronaldocedras@gmail.com>
2020-05-26 13:51:05 +0200

    added stack and scrollbar

--- app/views.py
+++ app/views.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
 @app.route('/sent', methods = ['POST', 'GET'])
 def send():
     result = request.form
