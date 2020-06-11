3c6cfa8f474c1faaf1f647b018a9e8996f9083d9
ronaldocerdras <ronaldocedras@gmail.com>
2020-06-11 20:02:15 +0200

    added env variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mail import Mail, Message
 import smtplib
+import os
 
 
 app = Flask(__name__)
@@ -10,9 +11,9 @@ app.config.update(
     MAIL_PORT = 465,
     MAIL_USE_SSL = True,
     MAIL_USERNAME = 'rcedras0010@gmail.com',
-    MAIL_PASSWORD = 'insert_enviorment_variable'
+    MAIL_PASSWORD = os.environ.get('PASS')
 )
-
+print(os.environ.get('PASS'))
 mail = Mail(app)
 
 from app import views
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='10.156.0.4')
+    app.run(debug=True)
 
-#added
